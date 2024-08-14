a1e486eca3f5ef2a08b669e5ce7f9cb5e989d253
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2024-08-14 22:44:19 +0700

    Add IRepository

---
+++ src/Core/Abstractions/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq.Expressions;
+
+namespace MoneyGroup.Core.Abstractions;
+
+public interface IRepository<TEntity>
+{
+    public Task<bool> AnyAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken = default);
+
+    public Task<TEntity?> FirstOrDefaultAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken = default);
+
+    public Task<TResult?> FirstOrDefaultAsync<TResult>(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken = default);
+
+    public Task<TEntity> AddAsync(TEntity entity, CancellationToken cancellationToken = default);
+
+    public Task<TResult> AddAsync<TResult>(TResult dto, CancellationToken cancellationToken = default);
+
+    public Task<TEntity> UpdateAsync(TEntity entity, CancellationToken cancellationToken = default);
+
+    public Task<TResult> UpdateAsync<TResult>(TResult dto, CancellationToken cancellationToken = default);
+
+    public Task RemoveAsync(TEntity entity, CancellationToken cancellationToken = default);
+}

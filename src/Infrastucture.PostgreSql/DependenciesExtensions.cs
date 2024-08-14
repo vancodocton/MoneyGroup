50336f347da4381a78419b5959fcb1cfac16de5b
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2024-08-15 00:08:35 +0700

    Register AppDbContext
    Add extension method for PostgreSQL setup in Infra

---
+++ src/Infrastucture.PostgreSql/DependenciesExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+using MoneyGroup.Infrastucture.Data;
+
+namespace MoneyGroup.Infrastucture.PostgreSql;
+public static class DependenciesExtensions
+{
+    private static string? MigrationsAssembly => typeof(DependenciesExtensions).Assembly.GetName().Name;
+
+    public static IServiceCollection AddApplicationDbContextNpgsql(this IServiceCollection services, string connectionString)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseNpgsql(connectionString, config =>
+            {
+                config.MigrationsAssembly(MigrationsAssembly);
+            }));
+
+        return services;
+    }
+}
--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -1,9 +1,15 @@
+using MoneyGroup.Infrastucture.PostgreSql;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("PostgreSqlConnection")
+    ?? throw new InvalidOperationException();
+builder.Services.AddApplicationDbContextNpgsql(connectionString);
 
 var app = builder.Build();
 

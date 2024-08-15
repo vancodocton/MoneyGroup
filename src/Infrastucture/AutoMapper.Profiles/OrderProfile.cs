ab0295fae5758116663f6a0879689ecd2b9c0097
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2024-08-15 22:41:48 +0700

    Add OrderProfile

---
+++ src/Infrastucture/AutoMapper.Profiles/OrderProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using AutoMapper.EquivalencyExpression;
+
+using MoneyGroup.Core.Entities;
+using MoneyGroup.Core.Models.Orders;
+
+namespace MoneyGroup.Infrastucture.AutoMapper.Profiles;
+internal class OrderProfile : Profile
+{
+    public OrderProfile()
+    {
+        CreateMap<OrderDto, Order>(MemberList.Source)
+            .ReverseMap();
+
+        CreateMap<ConsumerDto, OrderConsumer>(MemberList.Source)
+            .ForPath(dest => dest.ConsumerId, opt => opt.MapFrom(src => src.Id))
+            .ForMember(dest => dest.OrderId, opt => opt.UseDestinationValue())
+            .ReverseMap();
+    }
+}
---
+++ test/IntegrationTests/AutoMapper.Profiles/OrderProfileTest.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+
+using MoneyGroup.Infrastucture.AutoMapper.Profiles;
+
+namespace MoneyGroup.IntegrationTests.AutoMapper.Profiles;
+public class OrderProfileTest
+{
+    [Fact]
+    public void OrderProfile_ShouldHaveValidMappings()
+    {
+        // Arrange
+        var configuration = new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile<OrderProfile>();
+        });
+
+        // Assert
+        configuration.AssertConfigurationIsValid();
+    }
+}

0bc6c2ae164b6d3f3540d990f6ae93c9629e59fb
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2024-09-17 00:27:47 +0700

    TestHealthCheck

---
+++ test/FunctionalTests/TestHealthCheck.cs
@@ -0,0 +1,28 @@
+﻿using MoneyGroup.FunctionalTests.Fixture;
+
+namespace MoneyGroup.FunctionalTests;
+public class TestHealthCheck
+    : IClassFixture<WebApiFactory>
+{
+    private readonly WebApiFactory _webApiFactory;
+
+    public TestHealthCheck(WebApiFactory webApiFactory)
+    {
+        _webApiFactory = webApiFactory;
+    }
+
+    [Fact]
+    public async Task HealthCheck_ReturnsOk()
+    {
+        // Arrange
+        var client = _webApiFactory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync("/healthz");
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+        var responseString = await response.Content.ReadAsStringAsync();
+        Assert.Equal("Healthy", responseString);
+    }
+}

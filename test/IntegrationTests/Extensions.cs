0344829a661de65a39f1a9854c01d80b0e996dac
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2024-09-26 22:15:57 +0700

    Add integration tests for OrderRepository

---
+++ test/IntegrationTests/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace MoneyGroup.IntegrationTests;
+
+public static class Extensions
+{
+    private static readonly JsonSerializerOptions Options = new()
+    {
+        WriteIndented = true,
+        ReferenceHandler = ReferenceHandler.IgnoreCycles,
+    };
+
+    public static string ToJson<T>(this T obj)
+    {
+        return JsonSerializer.Serialize(obj, Options);
+    }
+}

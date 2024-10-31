1220efcb5919981dc2c6542655c0ea2407831e8e
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2024-10-31 23:39:16 +0700

    Add BasePaginationRequest for handling pagination

---
+++ src/WebApi/Endpoints/BasePaginationRequest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MoneyGroup.WebApi.Endpoints;
+
+public abstract class BasePaginationRequest
+{
+    protected BasePaginationRequest(int page, int size)
+    {
+        Page = page;
+        Size = size;
+    }
+    [FromQuery(Name = "p")]
+    public int Page { get; }
+
+    [FromQuery(Name = "s")]
+    public int Size { get; }
+}

ebd46147edf4fe98046aedf19c8b3a5cd456b339
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2024-10-31 23:29:39 +0700

    Add pagination support to repository pattern
    Added PaginationModel<T> to represent paginated data.
    
    Introduced GetByPageAsync methods in IRepository interface.
    
    Implemented pagination methods in EfRepository class.

---
+++ src/Core/Models/PaginationModel.cs
@@ -0,0 +1,12 @@
+﻿namespace MoneyGroup.Core.Models;
+
+public class PaginationModel<T>
+{
+    public required int Page { get; init; }
+
+    public required int Count { get; init; }
+
+    public required int Total { get; init; }
+
+    public required IEnumerable<T> Items { get; init; }
+}

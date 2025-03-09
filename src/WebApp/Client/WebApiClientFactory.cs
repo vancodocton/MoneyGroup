c5bb8f93b3aa6870dabf5508b7afe959ae4da2f4
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2025-03-09 09:26:39 +0700

    Register WebApiClient as transient service

---
+++ src/WebApp/Client/WebApiClientFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Kiota.Abstractions.Authentication;
+using Microsoft.Kiota.Http.HttpClientLibrary;
+
+namespace MoneyGroup.WebApi;
+
+public class WebApiClientFactory
+{
+    private readonly IAuthenticationProvider _authenticationProvider;
+    private readonly HttpClient _httpClient;
+
+    public WebApiClientFactory(HttpClient httpClient)
+    {
+        _authenticationProvider = new AnonymousAuthenticationProvider();
+        _httpClient = httpClient;
+    }
+
+    public WebApiClient GetClient()
+    {
+        return new WebApiClient(new HttpClientRequestAdapter(_authenticationProvider, httpClient: _httpClient));
+    }
+}

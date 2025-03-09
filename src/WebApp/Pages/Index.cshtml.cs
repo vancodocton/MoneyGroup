01d1731970d6c6bc0d956a793184bfa06adc05fb
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2025-03-09 09:48:00 +0700

    dnp

--- src/WebApp/Pages/Index.cshtml.cs
+++ src/WebApp/Pages/Index.cshtml.cs
@@ -1,19 +1,26 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using MoneyGroup.WebApi;
+using MoneyGroup.WebApi.Models;
 
 namespace MoneyGroup.WebApp.Pages;
 
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    private readonly WebApiClient _webApiClient;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ILogger<IndexModel> logger, WebApiClient webApiClient)
     {
         _logger = logger;
+        _webApiClient = webApiClient;
     }
 
-    public void OnGet()
-    {
+    public PaginationModelOfOrderDetailedDto? OrdersPaginatedModel { get; private set; }
 
+    public async Task OnGet()
+    {
+        _logger.LogInformation("OnGet called");
+        OrdersPaginatedModel = await _webApiClient.Api.Order.GetAsync() ?? throw new InvalidOperationException();
     }
 }

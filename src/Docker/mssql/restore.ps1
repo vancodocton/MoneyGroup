a49ae409f4a020406e0600238f5b3e6cd67f153a
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2025-03-20 23:09:22 +0700

    v3 separate

--- src/Docker/mssql/restore.ps1
+++ src/Docker/mssql/restore.ps1
@@ -7,4 +7,4 @@ sqlcmd -S localhost -U sa -P $MSSQL_SA_PASSWORD -C -Q "ALTER DATABASE [MoneyGrou
 sqlcmd -S localhost -U sa -P $MSSQL_SA_PASSWORD -C -Q "DROP DATABASE IF EXISTS MoneyGroup;"
 
 # Restore the database from the dump file
-sqlcmd -S localhost -U sa -P $MSSQL_SA_PASSWORD -C -i ./scripts/dump.sql
+sqlcmd -S localhost -U sa -P $MSSQL_SA_PASSWORD -C -i .\scripts\1.create-database.sql -i .\scripts\2.migrations.sql -i .\scripts\3.dump.sql
---
+++ src/Docker/mssql/scripts/1.create-database.sql
@@ -0,0 +1,6 @@
+USE [master]
+GO
+
+CREATE DATABASE [MoneyGroup]
+    COLLATE SQL_Latin1_General_CP1_CI_AS
+GO

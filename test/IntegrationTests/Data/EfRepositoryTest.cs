a781f86a5297565c7e9866f8c814e3a637a2f228
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2025-03-25 23:20:23 +0700

    Add EfRepositoryTest for OrderRepository pagination
    Introduce a new test class `EfRepositoryTest` in the
    `MoneyGroup.IntegrationTests.Data` namespace. This class
    includes two test methods to validate the pagination behavior
    of the `OrderRepository`, ensuring that the item counts
    returned match the expected values based on the current page
    and page size. The tests utilize an in-memory database
    context and seed it with sample order data for accurate
    testing.

---
+++ test/IntegrationTests/Data/EfRepositoryTest.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using MoneyGroup.Core.Entities;
+using MoneyGroup.Core.Models.Paginations;
+using MoneyGroup.Core.Services;
+using MoneyGroup.Infrastructure.Data;
+using MoneyGroup.IntegrationTests.Fixtures;
+
+namespace MoneyGroup.IntegrationTests.Data;
+
+public class EfRepositoryTest
+    : IClassFixture<EfRepositoryFixture>
+{
+    private readonly EfRepositoryFixture _dbContextFixture;
+
+    public EfRepositoryTest(EfRepositoryFixture efRepositoryFixture)
+    {
+        _dbContextFixture = efRepositoryFixture;
+    }
+
+    [Fact]
+    public async Task WhenNotLastPage_ThenItemsCountEqualPageSize()
+    {
+        // Arrange
+        int page = 1;
+        int size = 3;
+        int total = 5;
+
+        // Act
+        var model = await ActAsync(total, page, size);
+
+        // Assert
+        Assert.NotNull(model);
+        Assert.Equal(page, model.Page);
+        Assert.Equal(total, model.Total);
+        Assert.Equal(size, model.Count);
+    }
+
+    [Fact]
+    public async Task WhenLastPage_ThenItemsCountLessThanPageSize()
+    {
+        // Arrange
+        int page = 2;
+        int size = 3;
+        int total = 5;
+
+        // Act
+        var model = await ActAsync(total, page, size);
+
+        // Assert
+        Assert.NotNull(model);
+        Assert.Equal(page, model.Page);
+        Assert.Equal(total, model.Total);
+        Assert.True(model.Count < size);
+        Assert.Equal(2, model.Count); // total - (page - 1) * size
+    }
+
+    private async Task<PaginatedModel<Order>> ActAsync(int total, int page, int size)
+    {
+        // Arrange
+        var spec = new OrderPaginatedSpec(new PaginatedOptions(page, size));
+        await using var dbContext = _dbContextFixture.CreateDbContext();
+        await dbContext.Database.BeginTransactionAsync(TestContext.Current.CancellationToken);
+        await dbContext.Orders.ExecuteDeleteAsync(TestContext.Current.CancellationToken);
+        for (int i = 0; i < total; i++) // Seed 10 order
+        {
+            var orderDto = new Order
+            {
+                Title = "SeedOrder",
+                Description = $"SeedOrder{i}",
+                Total = 100.00m,
+                BuyerId = 1,
+                Participants = [
+                    new() { ParticipantId = 1 },
+                    new() { ParticipantId = 3 },
+                ],
+            };
+            dbContext.Add(orderDto);
+            await dbContext.SaveChangesAsync(TestContext.Current.CancellationToken);
+            dbContext.ChangeTracker.Clear();
+        }
+        var mapper = _dbContextFixture.Mapper;
+        var repository = new OrderRepository(dbContext, mapper);
+
+        // Act
+        return await repository.GetByPageAsync(spec, TestContext.Current.CancellationToken);
+    }
+}

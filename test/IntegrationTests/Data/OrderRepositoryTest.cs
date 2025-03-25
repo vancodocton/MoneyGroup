fe97f3fb3222beabab138c963ba433cc5b8be915
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2025-03-26 00:58:53 +0700

    Refactor OrderRepositoryTest

--- test/IntegrationTests/Data/OrderRepositoryTest.cs
+++ test/IntegrationTests/Data/OrderRepositoryTest.cs
@@ -18,6 +18,18 @@ public class OrderRepositoryTest
         _output = output;
     }
 
+    private static Order GetSeedOrder() => new()
+    {
+        Title = "AddOrder",
+        Description = "AddOrder",
+        Total = 100.00m,
+        BuyerId = 1,
+        Participants = [
+            new () { ParticipantId = 1 },
+            new () { ParticipantId = 3 },
+        ],
+    };
+
     [Fact]
     public async Task AddOrder_ValidDto_ShouldAddedOrder()
     {
@@ -65,22 +77,13 @@ public class OrderRepositoryTest
         await dbContext.Database.BeginTransactionAsync(TestContext.Current.CancellationToken);
         var mapper = _factory.Mapper;
         var repository = new OrderRepository(dbContext, mapper);
-        var orderDto = new OrderDto
-        {
-            Title = "AddOrder",
-            Description = "AddOrder",
-            Total = 100.00m,
-            BuyerId = 1,
-            Participants = [
-                new() { Id = 1 },
-                new() { Id = 3 },
-            ],
-        };
-        await repository.AddAsync(orderDto, TestContext.Current.CancellationToken);
+        var order = GetSeedOrder();
+        dbContext.Orders.Add(order);
+        await dbContext.SaveChangesAsync(TestContext.Current.CancellationToken);
         dbContext.ChangeTracker.Clear();
         var updateOrderDto = new OrderDto
         {
-            Id = orderDto.Id,
+            Id = order.Id,
             Title = "UpdatedOrder",
             Description = "UpdatedOrder",
             Total = 100.00m,
@@ -101,7 +104,7 @@ public class OrderRepositoryTest
         var updatedOrder = await dbContext.Orders
             .Include(o => o.Buyer)
             .Include(o => o.Participants.OrderBy(c => c.ParticipantId))
-            .FirstOrDefaultAsync(o => o.Id == orderDto.Id, TestContext.Current.CancellationToken);
+            .FirstOrDefaultAsync(o => o.Id == order.Id, TestContext.Current.CancellationToken);
         Assert.NotNull(updatedOrder);
         Assert.Equal(2, updatedOrder.BuyerId);
         Assert.Equal(updateOrderDto.Participants.Select(o => o.Id), updatedOrder.Participants.Select(c => c.ParticipantId));
@@ -115,27 +118,17 @@ public class OrderRepositoryTest
         await dbContext.Database.BeginTransactionAsync(TestContext.Current.CancellationToken);
         var mapper = _factory.Mapper;
         var repository = new OrderRepository(dbContext, mapper);
-        var newOrder = new Order
-        {
-            Title = "AddOrder",
-            Description = "AddOrder",
-            Total = 100.00m,
-            BuyerId = 1,
-            Participants = [
-                new() { ParticipantId = 1 },
-                new() { ParticipantId = 2 },
-                new() { ParticipantId = 3 },
-            ],
-        };
-        var addedOrder = await repository.AddAsync(newOrder, TestContext.Current.CancellationToken);
+        var order = GetSeedOrder();
+        dbContext.Orders.Add(order);
+        await dbContext.SaveChangesAsync(TestContext.Current.CancellationToken);
         dbContext.ChangeTracker.Clear();
 
         // Act
-        _output.WriteLine(addedOrder.ToJson());
-        await repository.RemoveAsync(addedOrder, TestContext.Current.CancellationToken);
+        await repository.RemoveAsync(order, TestContext.Current.CancellationToken);
+        _output.WriteLine(order.ToJson());
 
         // Assert
-        var deletedOrder = await dbContext.Orders.FindAsync([addedOrder.Id], TestContext.Current.CancellationToken);
+        var deletedOrder = await dbContext.Orders.FindAsync([order.Id], TestContext.Current.CancellationToken);
         Assert.Null(deletedOrder);
     }
 
@@ -147,24 +140,16 @@ public class OrderRepositoryTest
         await dbContext.Database.BeginTransactionAsync(TestContext.Current.CancellationToken);
         var mapper = _factory.Mapper;
         var repository = new OrderRepository(dbContext, mapper);
-        var newOrder = new Order
-        {
-            Title = "AddOrder",
-            Description = "AddOrder",
-            Total = 100.00m,
-            BuyerId = 1,
-            Participants = [
-                new() { ParticipantId = 1 },
-            ],
-        };
-        var addedOrder = await repository.AddAsync(newOrder, TestContext.Current.CancellationToken);
+        var order = GetSeedOrder();
+        dbContext.Orders.Add(order);
+        await dbContext.SaveChangesAsync(TestContext.Current.CancellationToken);
         dbContext.ChangeTracker.Clear();
 
         // Act
-        await repository.RemoveAsync(new Order { Id = addedOrder.Id }, TestContext.Current.CancellationToken);
+        await repository.RemoveAsync(new Order { Id = order.Id }, TestContext.Current.CancellationToken);
 
         // Assert
-        var deletedOrder = await dbContext.Orders.FindAsync([addedOrder.Id], TestContext.Current.CancellationToken);
+        var deletedOrder = await dbContext.Orders.FindAsync([order.Id], TestContext.Current.CancellationToken);
         Assert.Null(deletedOrder);
     }
 }

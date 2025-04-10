003e93625f9f92bdb7669c8ec2a88064e6a73a9e
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2025-04-10 17:04:20 +0700

    Init AgentApp

---
+++ src/AgentApp/Program.cs
@@ -0,0 +1,8 @@
+using MoneyGroup.AgentApp;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+
+await host.RunAsync();

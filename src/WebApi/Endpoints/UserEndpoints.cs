4e6eff8eb8cf5ee60f7798b557da52a5de7050e2
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2025-04-18 21:26:23 +0700

    Add user management endpoints and serialization support
    - Updated `AppJsonSerializerContext` for `UserDto` and `PaginatedModel<UserDto>`.
    - Created `UserEndpoints` for fetching users and user details with authorization.
    - Introduced `UserPaginatedRequest` for paginated user queries with validation.
    - Modified OpenAPI documentation to include new user endpoints and schemas.
    - Added `UserPaginatedRequestValidator` for request validation.
    - Registered `IUserService` and `IUserRepository` in `Program.cs`.
    - Expanded OpenAPI documentation with new schemas and tags for user endpoints.

---
+++ src/WebApi/Endpoints/UserEndpoints.cs
@@ -0,0 +1,64 @@
+﻿using System.Security.Claims;
+
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+using MoneyGroup.Core.Abstractions;
+using MoneyGroup.Core.Models.Paginations;
+using MoneyGroup.Core.Models.Users;
+
+using SharpGrip.FluentValidation.AutoValidation.Endpoints.Extensions;
+
+namespace MoneyGroup.WebApi.Endpoints;
+
+public static class UserEndpoints
+{
+    public static void MapUserEndpoints(this IEndpointRouteBuilder routes)
+    {
+        var group = routes.MapGroup("/api/User")
+            .AddFluentValidationAutoValidation()
+            .RequireAuthorization()
+            .WithTags("User");
+
+        group.MapGet("/", GetUsersAsync)
+        .WithName("GetUsers")
+        .WithOpenApi();
+
+        group.MapGet("/{id:int}", GetUserByIdAsync)
+        .WithName("GetUserById")
+        .WithOpenApi();
+
+        group.MapGet("/my", GetExecutingUserAsync)
+        .WithName("GetExecutingUser")
+        .WithOpenApi();
+    }
+
+    private static async Task<Results<Ok<PaginatedModel<UserDto>>, ValidationProblem>> GetUsersAsync([AsParameters] UserPaginatedRequest request, [FromServices] IUserService userService)
+    {
+        var users = await userService.GetUsersByPageAsync(request);
+        return TypedResults.Ok(users);
+    }
+
+    public static async Task<Results<Ok<UserDto>, NotFound>> GetUserByIdAsync(int id, [FromServices] IUserService userService, CancellationToken cancellationToken)
+    {
+        var user = await userService.GetUserByIdAsync(id, cancellationToken);
+        return user == null
+            ? TypedResults.NotFound()
+            : TypedResults.Ok(user);
+    }
+
+    public static async Task<Results<Ok<UserDto>, UnauthorizedHttpResult>> GetExecutingUserAsync([FromServices] IUserService userService, HttpContext httpContext)
+    {
+        var userEmail = httpContext.User.FindFirstValue(ClaimTypes.Email);
+
+        if (string.IsNullOrEmpty(userEmail))
+        {
+            return TypedResults.Unauthorized();
+        }
+
+        var user = await userService.GetUserByEmailAsync(userEmail, httpContext.RequestAborted);
+        return user == null
+            ? TypedResults.Unauthorized()
+            : TypedResults.Ok(user);
+    }
+}

1af4aa5d79fcb412d2e5b479f9eeb884d006aa9c
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2025-04-18 21:23:23 +0700

    Add IUserService and implementation

---
+++ src/Core/Models/Users/UserPaginatedOptions.cs
@@ -0,0 +1,13 @@
+﻿using MoneyGroup.Core.Models.Paginations;
+
+namespace MoneyGroup.Core.Models.Users;
+
+public class UserPaginatedOptions
+    : PaginatedOptions
+{
+    public UserPaginatedOptions(int page, int size) : base(page, size)
+    {
+    }
+
+    public virtual string? Keyword { get; init; }
+}
---
+++ src/Core/Services/UserService.cs
@@ -0,0 +1,31 @@
+﻿using MoneyGroup.Core.Abstractions;
+using MoneyGroup.Core.Entities;
+using MoneyGroup.Core.Models.Paginations;
+using MoneyGroup.Core.Models.Users;
+using MoneyGroup.Core.Specifications;
+
+namespace MoneyGroup.Core.Services;
+
+public class UserService(IUserRepository userRepository)
+    : IUserService
+{
+    private readonly IUserRepository _userRepository = userRepository;
+
+
+    public async Task<UserDto?> GetUserByEmailAsync(string email, CancellationToken cancellationToken = default)
+    {
+        return await _userRepository.FirstOrDefaultAsync<UserDto>(new UserByEmailSpec(email), cancellationToken);
+    }
+
+    /// <inheritdoc />
+    public async Task<UserDto?> GetUserByIdAsync(int id, CancellationToken cancellationToken = default)
+    {
+        return await _userRepository.FirstOrDefaultAsync<UserDto>(new EntityByIdSpec<User>(id), cancellationToken);
+    }
+
+    /// <inheritdoc />
+    public Task<PaginatedModel<UserDto>> GetUsersByPageAsync(UserPaginatedOptions options, CancellationToken cancellationToken = default)
+    {
+        return _userRepository.GetByPageAsync<UserDto>(new UserPaginatedSpec(options), cancellationToken);
+    }
+}

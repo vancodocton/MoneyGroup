c8d8e285c613a00b55582fabe8247d457fdffc18
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2025-09-24 14:39:38 +0700

    Add Adminer

--- src/AppHost/AppHost.cs
+++ src/AppHost/AppHost.cs
@@ -4,6 +4,11 @@ var mssqlServer = builder.AddSqlServer("sqlserver")
     .WithBindMount("../Infrastructure.SqlServer/Docker/scripts/", "/mssql-server-setup-scripts.d/")
     .WithLifetime(ContainerLifetime.Persistent);
 
+if (builder.ExecutionContext.IsRunMode)
+{
+    mssqlServer.WithAdminer(c => c.WithExplicitStart());
+}
+
 var mssqlDatabase = mssqlServer.AddDatabase("SqlServerConnection", "MoneyGroup");
 
 var webapi = builder.AddProject<Projects.MoneyGroup_WebApi>("moneygroup-webapi")

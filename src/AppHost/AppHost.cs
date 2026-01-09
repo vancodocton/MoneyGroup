880514a8ff8372653d033d0e2a8e8afc59627a30
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2026-01-09 21:24:36 +0700

    fix: Specify port for SQL Server in application builder

--- src/AppHost/AppHost.cs
+++ src/AppHost/AppHost.cs
@@ -1,11 +1,12 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var mssql = builder.AddSqlServer("mssql")
+var mssql = builder.AddSqlServer("mssql", port: 1435)
     .WithLifetime(ContainerLifetime.Persistent)
-    .WithBindMount("../Infrastructure.SqlServer/Docker/scripts", "/mssql-server-setup-scripts.d/")  
+    .WithBindMount("../Infrastructure.SqlServer/Docker/scripts", "/mssql-server-setup-scripts.d/")
     .AddDatabase("SqlServerConnection", "MoneyGroup");
 
 builder.AddProject<Projects.MoneyGroup_WebApi>("moneygroup-webapi")
+    .WaitFor(mssql)
     .WithReference(mssql);
 
 await builder.Build().RunAsync();

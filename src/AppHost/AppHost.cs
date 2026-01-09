7cce7d2e996792acbaa6d7e89caeec8e566f4cca
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2026-01-09 21:03:46 +0700

    fix: Correct path separator in SQL Server bind mount configuration

--- src/AppHost/AppHost.cs
+++ src/AppHost/AppHost.cs
@@ -2,7 +2,7 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var mssql = builder.AddSqlServer("mssql")
     .WithLifetime(ContainerLifetime.Persistent)
-    .WithBindMount("..\\Infrastructure.SqlServer\\Docker\\scripts", "/mssql-server-setup-scripts.d/")
+    .WithBindMount("../Infrastructure.SqlServer/Docker/scripts", "/mssql-server-setup-scripts.d/")  
     .AddDatabase("SqlServerConnection", "MoneyGroup");
 
 builder.AddProject<Projects.MoneyGroup_WebApi>("moneygroup-webapi")

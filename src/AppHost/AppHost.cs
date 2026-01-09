906c561dc7bbe0cd6dd2405dfd662e9d8b2ce9b3
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2026-01-09 21:03:25 +0700

    fix: Correct database connection parameters for SQL Server setup

--- src/AppHost/AppHost.cs
+++ src/AppHost/AppHost.cs
@@ -3,7 +3,7 @@ var builder = DistributedApplication.CreateBuilder(args);
 var mssql = builder.AddSqlServer("mssql")
     .WithLifetime(ContainerLifetime.Persistent)
     .WithBindMount("..\\Infrastructure.SqlServer\\Docker\\scripts", "/mssql-server-setup-scripts.d/")
-    .AddDatabase("MoneyGroup");
+    .AddDatabase("SqlServerConnection", "MoneyGroup");
 
 builder.AddProject<Projects.MoneyGroup_WebApi>("moneygroup-webapi")
     .WithReference(mssql);

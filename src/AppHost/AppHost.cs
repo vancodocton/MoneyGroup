633617b9e1addfee831d8a7f40ef3e65995e1ad3
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2026-05-25 22:55:00 +0700

    fix: setup Node.js for moneygroup-clientapp
    with npm caching

--- src/AppHost/AppHost.cs
+++ src/AppHost/AppHost.cs
@@ -1,4 +1,6 @@
 #pragma warning disable ASPIRECOMPUTE003 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+#pragma warning disable ASPIREPIPELINES003 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+#pragma warning disable ASPIREJAVASCRIPT001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
 
 var builder = DistributedApplication.CreateBuilder(args);
 
@@ -11,11 +13,20 @@ var mssql = builder.AddSqlServer("mssql", port: 1435)
 
 var webapi = builder.AddProject<Projects.MoneyGroup_WebApi>("moneygroup-webapi")
     .WithContainerRegistry(ghcr)
+    .WithImagePushOptions(options =>
+    {
+        options.Options.RemoteImageTag = builder.Configuration["tag"];
+    })
     .WaitFor(mssql)
     .WithReference(mssql);
 
 builder.AddViteApp("moneygroup-clientapp", "../ClientApp")
+    .PublishAsStaticWebsite()
     .WithContainerRegistry(ghcr)
+    .WithImagePushOptions(options =>
+    {
+        options.Options.RemoteImageTag = builder.Configuration["tag"];
+    })
     .WithReference(webapi)
     .WaitFor(webapi);
 

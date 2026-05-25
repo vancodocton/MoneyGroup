a67e99823c127818a44b0cefd73f420d23ac762b
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2026-05-25 21:56:52 +0700

    feat: add GitHub container registry configuration for webapi and clientapp

--- src/AppHost/AppHost.cs
+++ src/AppHost/AppHost.cs
@@ -1,4 +1,8 @@
+#pragma warning disable ASPIRECOMPUTE003 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+
 var builder = DistributedApplication.CreateBuilder(args);
+
+var ghcr = builder.AddContainerRegistry("ghcr", "ghcr.io");
 
 var mssql = builder.AddSqlServer("mssql", port: 1435)
     .WithLifetime(ContainerLifetime.Persistent)
@@ -6,10 +10,12 @@ var mssql = builder.AddSqlServer("mssql", port: 1435)
     .AddDatabase("SqlServerConnection", "MoneyGroup");
 
 var webapi = builder.AddProject<Projects.MoneyGroup_WebApi>("moneygroup-webapi")
+    .WithContainerRegistry(ghcr)
     .WaitFor(mssql)
     .WithReference(mssql);
 
 builder.AddViteApp("moneygroup-clientapp", "../ClientApp")
+    .WithContainerRegistry(ghcr)
     .WithReference(webapi)
     .WaitFor(webapi);
 

734a688b8dca42736d3bcd281f9982c56fe45302
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2026-05-27 17:36:05 +0700

    Switch to Azure SQL for database in AppHost
    Added Aspire.Hosting.Azure.Sql package to solution and updated AppHost.cs to use AddAzureSqlServer instead of AddSqlServer. The SQL resource now runs as a container with the same configuration, enabling use of Azure SQL for the application's database.

--- src/AppHost/AppHost.cs
+++ src/AppHost/AppHost.cs
@@ -2,8 +2,6 @@
 #pragma warning disable ASPIREPIPELINES003 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
 #pragma warning disable ASPIREJAVASCRIPT001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
 
-using Aspire.Hosting.Azure.AppContainers;
-
 using Microsoft.Extensions.Hosting;
 
 var builder = DistributedApplication.CreateBuilder(args);
@@ -13,9 +11,12 @@ var ghcr = builder
 
 var acEnv = builder.AddAzureContainerAppEnvironment("moneygroup-env");
 
-var mssql = builder.AddSqlServer("mssql", port: 1435)
-    .WithLifetime(ContainerLifetime.Persistent)
-    .WithBindMount("../Infrastructure.SqlServer/Docker/scripts", "/mssql-server-setup-scripts.d/")
+var mssql = builder
+    .AddAzureSqlServer("moneygroup-sqlserver")
+    .RunAsContainer(config => config
+        .WithHostPort(1435)
+        .WithLifetime(ContainerLifetime.Persistent)
+        .WithBindMount("../Infrastructure.SqlServer/Docker/scripts", "/mssql-server-setup-scripts.d/"))
     .AddDatabase("SqlServerConnection", "MoneyGroup");
 
 var webapi = builder.AddProject<Projects.MoneyGroup_WebApi>("moneygroup-webapi")
